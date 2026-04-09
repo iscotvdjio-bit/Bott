92d9236ccc9f04847afeccbad4460884dd00e80e
Iscoo Tavadjio <iscotvdjio@gmail.com>
2026-04-09 19:53:33 +0900

    Update database.js

--- database.js
+++ database.js
@@ -1,34 +1,39 @@
 const fs = require("fs");
+const path = "./database.json";
 
-let db = {};
+let data = {};
 
-if (fs.existsSync("./database.json")) {
-  db = JSON.parse(fs.readFileSync("./database.json"));
+// load awal
+if (fs.existsSync(path)) {
+  data = JSON.parse(fs.readFileSync(path));
 }
 
+// save function
 function save() {
-  fs.writeFile("./database.json", JSON.stringify(db, null, 2), () => {});
+  fs.writeFileSync(path, JSON.stringify(data, null, 2));
 }
 
 module.exports = {
   get(id, key) {
-    if (!db[id]) db[id] = {};
-    return db[id][key] || 0;
+    if (!data[id]) return null;
+    return data[id][key] || 0;
   },
 
   set(id, key, value) {
-    if (!db[id]) db[id] = {};
-    db[id][key] = value;
+    if (!data[id]) data[id] = {};
+    data[id][key] = value;
     save();
   },
 
   add(id, key, value) {
-    if (!db[id]) db[id] = {};
-    db[id][key] = (db[id][key] || 0) + value;
+    if (!data[id]) data[id] = {};
+    if (!data[id][key]) data[id][key] = 0;
+
+    data[id][key] += value;
     save();
   },
 
   all() {
-    return db;
+    return data;
   }
 };
